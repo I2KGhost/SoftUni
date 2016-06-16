0764a51ef0246fe699aaf272fabed0dc33232f95
Ivaylo <ivo_i2k@abv.bg>
2016-06-16 13:51:08 +0300

    First steps in programming
    Homework

---
+++ Square of Stars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Square_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string spaces = new string(' ', n - 2);   // създава ни стринг с n-2 интервала
+            string startingStars = new string('*', n);  //създава ни стринг с n звезди
+
+            Console.WriteLine(startingStars);   // отпечатва първи ред
+
+            for (int i = 1; i <= (n - 2); i++)   //отпечатва от втори до n-1 ред
+            {
+                Console.WriteLine("*" + spaces + "*");
+            }
+            Console.WriteLine(startingStars);   // отпечатва последния ред
+
+        }
+    }
+}
---
+++ TriangleOf55Stars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace TriangleOf55Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                string stars = new string('*', i);
+                Console.WriteLine(stars);
+            }
+        }
+    }
+}
